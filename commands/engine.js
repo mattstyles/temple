4387e2e5713081c7ebb7c1e8c1388781ca2311ff
Matt Styles <matt@veryfizzyjelly.com>
2016-06-02 08:27:16 +0100

    #add set engine config values and show engines

--- commands/engine.js
+++ commands/engine.js
@@ -6,6 +6,7 @@
  *
  * @example
  *   temple engine --all
+ *   temple engine --all --json
  *   temple engine hogan
  *   temple engine hogan.extensions "hbs hogan hulk"
  *   temple engine --install hogan
@@ -16,20 +17,66 @@ const usage = require( '../lib/usage' )
 const conf = require( '../lib/conf' )()
 const pkg = require( '../package.json' )
 
+const ENGINE_KEY = 'engines'
+
 /**
  * Manages behaviours surrounding the templating engines
  */
 module.exports = function( opts ) {
-  let engines = conf.get( 'engines' )
+  let engines = conf.get( ENGINE_KEY )
 
   console.log( 'engines' )
   console.log( engines )
+
   if ( opts.all ) {
     if ( opts.json ) {
-      process.stdout.write( JSON.stringify( ))
+      process.stdout.write( JSON.stringify( engines ) )
+      return
     }
     // @TODO make tabular
-    engines.forEach( )
+    engines
+      .map( engine => engine.name + '\n' )
+      .forEach( engine => {
+        process.stdout.write( engine )
+      })
     return
   }
+
+  if ( !opts._ || !opts._.length ) {
+    usage( 'engine' )
+    return
+  }
+
+  let key = opts._[ 0 ]
+  let value = opts._[ 1 ]
+
+  // Try getting the engine data
+  if ( !value ) {
+    let engine = engines.find( engine => engine.name === key )
+
+    if ( !engine ) {
+      console.log( `${ pkg.shortname }: Can not find specified engine` )
+      console.log( `See '${ pkg.shortname } engine --help'` )
+      return
+    }
+
+    // @TODO make tabular
+    process.stdout.write( JSON.stringify( engine ) )
+    return
+  }
+
+  // Otherwise try setting the specific key for the engine
+  let keypath = key.split( '.' )
+  if ( keypath.length === 1 ) {
+    console.log( `${ pkg.shortname }: Invalid keypath` )
+    console.log( `${ pkg.shortname }: Keypath should be of the format engine.key` )
+    console.log( `See '${ pkg.shortname } engine --help'` )
+    return
+  }
+
+  // Find engine and set the respective key
+  let engine = engines.find( engine => engine.name === keypath[ 0 ] )
+  engine[ keypath[ 1 ] ] = value
+
+  conf.set( ENGINE_KEY, engines )
 }
