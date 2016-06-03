b4f2517c97ed38222f91e9f822a1eb18eb407c17
Matt Styles <matt@veryfizzyjelly.com>
2016-06-03 07:48:03 +0100

    #update cli engine delete and show all

--- commands/engine.js
+++ commands/engine.js
@@ -22,84 +22,104 @@ const spawn = require( 'child_process' ).spawn
 const usage = require( '../lib/usage' )
 const conf = require( '../lib/conf' )()
 const pkg = require( '../package.json' )
-const core = require( '../lib/engine' )
+const engineCore = require( '../lib/engine' )
 
 const ENGINE_KEY = 'engines'
+const core = engineCore( conf.get( ENGINE_KEY ) )
 
 /**
  * Manages behaviours surrounding the templating engines
  */
-module.exports = function( opts ) {
-  /**
-   * Show all engine data
-   */
-  if ( opts.all ) {
-    core.showAll( opts )
-    return
-  }
+// module.exports = function( opts ) {
+//   /**
+//    * Show all engine data
+//    */
+//   if ( opts.all ) {
+//     core.showAll( opts )
+//     return
+//   }
+//
+//   /**
+//    * Install specific engine if it exists
+//    */
+//   if ( opts.install ) {
+//     core.install( opts._[ 0 ] || opts.install )
+//     return
+//   }
+//
+//   /**
+//    * Remove an engine
+//    */
+//   if ( opts.delete ) {
+//     core.remove( opts._[ 0 ] || opts.delete )
+//     return
+//   }
+//
+//   /**
+//    * Show help on 'temple engine'
+//    */
+//   if ( !opts._ || !opts._.length ) {
+//     usage( 'engine' )
+//     return
+//   }
+//
+//   /**
+//    * Handle getting and setting engine conf data
+//    */
+//   let engines = conf.get( ENGINE_KEY )
+//   let key = opts._[ 0 ]
+//   let value = opts._[ 1 ]
+//
+//   // Try getting the engine data
+//   if ( !value && process.stdin.isTTY ) {
+//     let engine = engines.find( engine => engine.name === key )
+//
+//     if ( !engine ) {
+//       console.log( `${ pkg.shortname }: Can not find specified engine` )
+//       console.log( `See '${ pkg.shortname } engine --help'` )
+//       return
+//     }
+//
+//     // @TODO make tabular
+//     process.stdout.write( JSON.stringify( engine ) )
+//     return
+//   }
+//
+//   // Try setting the specific key for the engine
+//   let keypath = key.split( '.' )
+//
+//   // Handle setting the whole meta for an engine
+//   if ( keypath.length === 1 ) {
+//     core.write( keypath[ 0 ] )
+//     return
+//   }
+//
+//   // Handle variadic
+//   if ( opts._.length > 2 ) {
+//     value = opts._.slice( 1, opts._.length )
+//   }
+//
+//   // Handle setting a specific key
+//   let engine = engines.find( engine => engine.name === keypath[ 0 ] )
+//   engine[ keypath[ 1 ] ] = value
+//   conf.set( ENGINE_KEY, engines )
+// }
 
-  /**
-   * Install specific engine if it exists
-   */
-  if ( opts.install ) {
-    core.install( opts._[ 0 ] || opts.install )
-    return
-  }
 
-  /**
-   * Remove an engine
-   */
+module.exports = function engine( opts ) {
+
   if ( opts.delete ) {
-    core.remove( opts._[ 0 ] || opts.delete )
+    let engines = core.remove( opts._[ 0 ] || opts.delete )
+    conf.set( ENGINE_KEY, engines )
     return
   }
 
-  /**
-   * Show help on 'temple engine'
-   */
-  if ( !opts._ || !opts._.length ) {
-    usage( 'engine' )
+  if ( opts.all ) {
+    core.showAll({
+      json: opts.json
+    })
     return
   }
 
-  /**
-   * Handle getting and setting engine conf data
-   */
-  let engines = conf.get( ENGINE_KEY )
-  let key = opts._[ 0 ]
-  let value = opts._[ 1 ]
 
-  // Try getting the engine data
-  if ( !value && process.stdin.isTTY ) {
-    let engine = engines.find( engine => engine.name === key )
-
-    if ( !engine ) {
-      console.log( `${ pkg.shortname }: Can not find specified engine` )
-      console.log( `See '${ pkg.shortname } engine --help'` )
-      return
-    }
-
-    // @TODO make tabular
-    process.stdout.write( JSON.stringify( engine ) )
-    return
-  }
-
-  // Try setting the specific key for the engine
-  let keypath = key.split( '.' )
-
-  // Handle setting the whole meta for an engine
-  if ( keypath.length === 1 ) {
-    core.write( keypath[ 0 ] )
-    return
-  }
-
-  // Handle variadic
-  if ( opts._.length > 2 ) {
-    value = opts._.slice( 1, opts._.length )
-  }
-
-  // Handle setting a specific key
-  let engine = engines.find( engine => engine.name === keypath[ 0 ] )
-  engine[ keypath[ 1 ] ] = value
-  conf.set( ENGINE_KEY, engines )
 }
