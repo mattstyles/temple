943f0a5755b9add83cbfdbb7db5bd40bfe391a4d
Matt Styles <matt@veryfizzyjelly.com>
2016-06-08 09:56:01 +0100

    #add tabular list output

--- commands/list.js
+++ commands/list.js
@@ -12,6 +12,11 @@
 const fs = require( 'fs' )
 const path = require( 'path' )
 const store = require( '../lib/store' )
+const conf = require( '../lib/conf' )()
+const Table = require( 'cli-table' )
+
+const ENGINE_KEY = 'engines'
+const engineCore = require( '../lib/engine' )( conf.get( ENGINE_KEY ) )
 
 /**
  * Lists all templates currently held by the store
@@ -23,11 +28,32 @@ module.exports = function list( opts ) {
   let templates = store( opts.dataDir || null )
 
   // @TODO make tabular on --json option supplied
-  templates.getAll()
-    .map( name => name + '\n' )
-    .forEach( name => {
-      // @TODO use lib/template show function
-      // @TODO currently showing directories, should show only files
-      process.stdout.write( name )
+  let tmpl = templates.getAll()
+    .map( filename => {
+      let name = filename.replace( path.extname( filename ), '' )
+      let engine = engineCore.find( path.extname( filename ).replace( /^\./, '' ) )
+      return {
+        name: name,
+        filename: filename,
+        engine: engine ? engine.name : 'unspecified'
+      }
     })
+
+  if ( opts.json ) {
+    process.stdout.write( JSON.stringify( tmpl ) )
+    return
+  }
+
+  let table = new Table({
+    chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+           , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+           , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
+           , 'right': '' , 'right-mid': '' , 'middle': ' ' },
+    style: { 'padding-left': 0, 'padding-right': 0 }
+  })
+
+  table.push([ 'NAME', 'FILENAME', 'ENGINE' ])
+  table.push( ...tmpl.map( t => [ t.name, t.filename, t.engine ] ) )
+
+  console.log( table.toString() )
 }
